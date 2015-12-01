ab100de9461c2c40496ae25721e120c46c5a1f9b
Bram <bobbiy@users.noreply.github.com>
2015-12-01 18:24:34 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,18 +7,15 @@ AIO_key = <AIO_key>
 AIO_username = <AIO_username>
 AIO_feedname = <feedname>
 
+--connect to wifi
 wifi.setmode(wifi.STATION)
 wifi.sta.config(username,password)
 
-
-function HexadecimalToColor(x)
+function HexToColor(x)
     if type(x) == 'number' then
         x = string.format('%x+', x):upper()
     end
         x = x:match'%x+':upper()
-    if x:len() ~= 6 then
-        return error('Invalid hexadecimal color code: ' .. x)
-    end
     return {
         r = loadstring('return 0x'..x:sub(1,2))();
         g = loadstring('return 0x'..x:sub(3,4))();
@@ -29,7 +26,7 @@ end
 m = mqtt.Client(AIO_key, 120, AIO_username, AIO_key)
 m:on("message", function(conn, topic, data)
     if data ~= nil then
-        local color = HexadecimalToColor(string.gsub(data, "#", "", 1))
+        local color = HexToColor(string.gsub(data, "#", "", 1))
         ws2812.writergb(4, string.char(color.r, color.g, color.b):rep(3))
     end
 end)
